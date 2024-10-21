26dcb188f2844a2d6c2bf4956e62f3eb48dadf68
RahulxRay <rr.rayrahul@gmail.com>
2024-10-21 12:43:55 +0100

    added inventory to main

---
+++ Assets/Scripts/Inventory/PlayerInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomPlayerInput : MonoBehaviour
+{
+    void Update()
+    {
+        for (int i = 1; i <= 7; i++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha0 + i))
+            {
+                int slotIndex = i;
+                Debug.Log($"Key {i} pressed. Selecting slot {slotIndex}.");
+                Inventory.Instance.SelectSlot(slotIndex);
+            }
+        }
+    }
+}
