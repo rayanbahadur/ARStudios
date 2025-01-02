5b92389a82c3ed9a7e18494aabc42db3d0f7f035
RahulxRay <rr.rayrahul@gmail.com>
2025-01-02 00:44:16 +0000

    Inventory system implemented

--- Assets/Scripts/level 5/SimpleFpsController.cs
+++ Assets/Scripts/level 5/SimpleFpsController.cs
@@ -33,6 +33,15 @@ public class SimpleFpsController : MonoBehaviour
         LookAround();
         Move();
         Jump();
+        for (int i = 1; i <= 7; i++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha0 + i))
+            {
+                int slotIndex = i;
+                Debug.Log($"Key {i} pressed. Selecting slot {slotIndex}.");
+                Inventory.Instance.SelectSlot(slotIndex);
+            }
+        }
         // Optionally: FlipOrientation(); // see example below
     }
 
