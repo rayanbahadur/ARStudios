3e8a2aa84ea916846e322bd094665be68da5980c
Abdullah Zulfiqar <usamash34@gmail.com>
2025-01-03 23:51:21 +0000

    Add poisoned health bar look

--- Assets/Scripts/PlayerHUD/HealthBar.cs
+++ Assets/Scripts/PlayerHUD/HealthBar.cs
@@ -6,11 +6,16 @@ using TMPro;
 
 public class HealthBar : MonoBehaviour
 {
-
     public Slider slider;
     public Gradient gradient;
     public Image fill;
     public TextMeshProUGUI healthText;
+    public Image heartImage; 
+    public Sprite poisonHeartSprite; 
+    public Sprite normalHeartSprite; 
+    public Gradient poisonGradient; 
+
+    private bool isPoisoned = false; // Track the poison status
 
     // Set the max health of the player
     public void SetMaxHealth(int health) 
@@ -29,5 +34,22 @@ public class HealthBar : MonoBehaviour
 
         fill.color = gradient.Evaluate(slider.normalizedValue);
         healthText.text = "HEALTH: " + health;
+    }
+
+    // Toggle poison effect
+    public void TogglePoisonEffect()
+    {
+        isPoisoned = !isPoisoned;
+
+        if (isPoisoned)
+        {
+            fill.color = poisonGradient.Evaluate(slider.normalizedValue);
+            heartImage.sprite = poisonHeartSprite;
+        }
+        else
+        {
+            fill.color = gradient.Evaluate(slider.normalizedValue);
+            heartImage.sprite = normalHeartSprite;
+        }
     }
 }
