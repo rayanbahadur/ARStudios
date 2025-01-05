ca12913e7c6771634b3c1a4dd76963911eda089c
Abdullah Zulfiqar <usamash34@gmail.com>
2025-01-05 09:22:17 +0000

    Create Taskbar UI

---
+++ Assets/Scripts/PlayerHUD/ProgressBar/TaskProgressBar.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TaskProgressBar : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+    public TextMeshProUGUI progressText;
+
+
+    // Set the max progress of the player
+    public void SetMaxProgress(int progressPercentage) 
+    {
+        slider.maxValue = progressPercentage;
+        slider.value = progressPercentage;
+
+        fill.color = gradient.Evaluate(1f);
+        progressText.text = progressPercentage + "%";
+    }
+
+    // Set the progress of the player
+    public void SetProgress(int progressPercentage)
+    {
+        slider.value = progressPercentage;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+        progressText.text = progressPercentage + "%";
+    }
+}
