8eefb05c81ca08678176dcca4fbe22370120d134
Abdullah Zulfiqar <usamash34@gmail.com>
2025-01-05 09:33:32 +0000

    Add Task Progress To Player

---
+++ Assets/Scripts/PlayerHUD/ProgressBar/PlayerProgress.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProgress : MonoBehaviour
+{
+
+    public int maxProgress = 100;
+    public int currentProgress;
+    public TaskProgressBar taskProgressBar; // Reference to the TaskProgressBar component
+
+    void Start()
+    {
+        currentProgress = 0;
+        taskProgressBar.SetMaxProgress(maxProgress); // Initialize the task progress bar
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Test taking damage
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            AddProgress(10);
+        }
+    }
+
+    public void AddProgress(int progress)
+    {
+        currentProgress += progress;
+        if (currentProgress > maxProgress)
+        {
+            currentProgress = maxProgress;
+        }
+        taskProgressBar.SetProgress(currentProgress); // Update the task progress bar
+    }
+}
